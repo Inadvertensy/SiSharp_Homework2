cc7a0042937679d327d365698ee083302f7fd169
1 1 <bobra2@ya.ru>
2022-08-23 09:44:14 +0300

    task 10 completed

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на 
+//вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Please enter three digit number");
+int number=Convert.ToInt32(Console.ReadLine());
+
+//if (ThrowResult >= 5 && ThrowResult <= 9)
+
+if (number>99 && number<1000){
+
+int firstDigit=number/100;
+int lastDigit=number%10;
+int result=(number-lastDigit-firstDigit*100)/10;
+    Console.WriteLine($"Second digit in the number is {result}");
+
+    //System.Console.WriteLine($"Number{number1} Max {max}");
+}
+else
+{
+    Console.WriteLine("Number is not three-digit");
+}
---
+++ task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Please enter number");
+int number=Convert.ToInt32(Console.ReadLine());
+if(number>100){
+Console.WriteLine(number%100);
+}
+else{
+Console.WriteLine($"Number {number} does not have the third digit");
+}
+
