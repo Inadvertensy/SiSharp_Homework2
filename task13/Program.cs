a1bca08ed074119287d93c1dada08803d0d1ca2d
1 1 <bobra2@ya.ru>
2022-08-24 08:39:19 +0300

    task 13 completed

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,10 +5,19 @@
 // 32679 -> 6
 Console.WriteLine("Please enter number");
 int number=Convert.ToInt32(Console.ReadLine());
-if(number>100){
-Console.WriteLine(number%100);
-}
-else{
+int numberAbs=Math.Abs(number);
+if(numberAbs<100){
 Console.WriteLine($"Number {number} does not have the third digit");
 }
-
+else{
+if(numberAbs<1000){
+    int result=numberAbs/100;
+    Console.WriteLine($"third digit in number {number} is {result}");
+}
+else{
+  //  Console.WriteLine(number/1000);
+    int div=numberAbs/1000;
+    int result=(numberAbs-div*1000)/100;
+    Console.WriteLine($"third digit in number{number} is {result}");
+}
+}
